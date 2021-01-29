f79013744e3481ffdd1f0b2018f58a5a34a99cd0
Rodrigo Ramos <rodrigoramos@Mac-mini-de-Rodrigo.local>
2021-01-29 12:42:31 -0300

    up

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -37,48 +37,27 @@ export default function Dashboard(props) {
         <div className="section-2">
           <h1>Total de Receitas:</h1>
           <h3>
-            {cashIn ?
-              parseFloat(
-                cashIn
-                  .map((data) => data.value.replace("R$", "").replace(".", ""))
-                  .reduce((acumulator, currentValue) => {
-                    return parseFloat(acumulator) + parseFloat(currentValue);
-                  })
-              ).toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              }):"R$ 0,00"}
+            {cashIn
+              ? parseFloat(
+                  cashIn
+                    .map((data) =>
+                      data.value.replace("R$", "").replace(".", "")
+                    )
+                    .reduce((acumulator, currentValue) => {
+                      return parseFloat(acumulator) + parseFloat(currentValue);
+                    })
+                ).toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })
+              : "R$ 0,00"}
           </h3>
         </div>
         <div className="section-1">
           <h1>Total de Gastos:</h1>
           <h3>
-            {cashOut ?
-              parseFloat(
-                cashOut
-                  .map((data) => data.value.replace("R$", "").replace(".", ""))
-                  .reduce((acumulator, currentValue) => {
-                    return parseFloat(acumulator) + parseFloat(currentValue);
-                  })
-              ).toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              }) : "R$ 0,00"}
-          </h3>
-        </div>
-      </div>
-      <div className="main-calendar">
-        <h1>Balanço:</h1>
-        <div className="cards-place">
-          <h5>
-            {cashOut &&
-              cashIn ?
-              parseFloat(
-                cashIn
-                  .map((data) => data.value.replace("R$", "").replace(".", ""))
-                  .reduce((acumulator, currentValue) => {
-                    return parseFloat(acumulator) + parseFloat(currentValue);
-                  }) -
+            {cashOut
+              ? parseFloat(
                   cashOut
                     .map((data) =>
                       data.value.replace("R$", "").replace(".", "")
@@ -86,10 +65,76 @@ export default function Dashboard(props) {
                     .reduce((acumulator, currentValue) => {
                       return parseFloat(acumulator) + parseFloat(currentValue);
                     })
-              ).toLocaleString("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              }) : "R$ 0,00"}
+                ).toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })
+              : "R$ 0,00"}
+          </h3>
+        </div>
+      </div>
+      <div className="main-calendar">
+        <h1>Balanço:</h1>
+        <div className="cards-place">
+          <h5>
+            {cashOut && cashIn
+              ? parseFloat(
+                 parseFloat( cashIn
+                    .map((data) =>
+                      data.value.replace("R$", "").replace(".", "")
+                    )
+                    .reduce((acumulator, currentValue) => {
+                      return parseFloat(acumulator) + parseFloat(currentValue);
+                    }) )-
+                    parseFloat(cashOut
+                      .map((data) =>
+                        data.value.replace("R$", "").replace(".", "")
+                      )
+                      .reduce((acumulator, currentValue) => {
+                        return (
+                          parseFloat(acumulator) + parseFloat(currentValue)
+                        );
+                      }))
+                ).toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })
+              : ""}
+
+            {cashOut && !cashIn
+              ? parseFloat(
+                  0 -
+                    parseFloat(cashOut
+                      .map((data) =>
+                        data.value.replace("R$", "").replace(".", "")
+                      )
+                      .reduce((acumulator, currentValue) => {
+                        return (
+                          parseFloat(acumulator) + parseFloat(currentValue)
+                        );
+                      }))
+                ).toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })
+              : ""}
+
+            {!cashOut && cashIn
+              ? parseFloat(
+                    cashIn
+                      .map((data) =>
+                        data.value.replace("R$", "").replace(".", "")
+                      )
+                      .reduce((acumulator, currentValue) => {
+                        return (
+                          parseFloat(acumulator) + parseFloat(currentValue)
+                        );
+                      })
+                ).toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })
+              : ""}
           </h5>
         </div>
       </div>
